bb8eaf163c99cf9b0af7ff2bc8731dc40a05f8eb
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-06-15 11:49:56 +0900

    Feat: create NavigationLink with navigationDestination

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -7,24 +7,22 @@
 
 import SwiftUI
 
-struct NavigationView: View {
-    let number: Int
-    var body: some View {
-        Text("\(number) NavigationView")
-    }
-    init(number: Int) {
-        self.number = number
-        print("\(number) NavigationView created")
-    }
+struct Person: Hashable {
+    var id = UUID()
+    var name: String
 }
 
 struct ContentView: View {
     var body: some View {
         NavigationStack {
             List(1..<51) { num in
-                NavigationLink("\(num) NavigationView") {
-                    NavigationView(number: num)
-                }
+                NavigationLink("\(num)", value: num)
+                    .navigationDestination(for: Int.self, destination: { selection in
+                        Text("You selected \(selection)")
+                    })
+                    .navigationDestination(for: Person.self, destination: { person in
+                        Text("You selected \(person)")
+                    })
             }
         }
     }

8ced510078003ade0aae586122eb3fdb2704c3ee
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-06-17 16:18:01 +0900

    Feat: save path by create PathStore

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -7,27 +7,55 @@
 
 import SwiftUI
 
+@Observable
+class PathStore {
+    var path: NavigationPath {
+        didSet {
+            save()
+        }
+    }
+
+    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
+
+    init() {
+        if let data = try? Data(contentsOf: savePath) {
+            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
+                path = NavigationPath(decoded)
+                return
+            }
+        }
+
+        // Start with an empty path.
+        path = NavigationPath()
+    }
+
+    func save() {
+        guard let representation = path.codable else { return }
+
+        do {
+            let data = try JSONEncoder().encode(representation)
+            try data.write(to: savePath)
+        } catch {
+            print("Failed to save navigation data")
+        }
+    }
+}
+
 struct NavigationView: View {
     var num: Int
-    @Binding var path: NavigationPath
     var body: some View {
         NavigationLink("Going to random NavigationView", value: Int.random(in: 0...1000))
             .navigationTitle("number \(num)")
-            .toolbar {
-                Button("Going home") {
-                    path = NavigationPath()
-                }
-            }
     }
 }
 
 struct ContentView: View {
-    @State private var path = NavigationPath()
+    @State private var pathStore = PathStore()
     var body: some View {
-        NavigationStack(path: $path) {
-            NavigationView(num: 0, path: $path)
+        NavigationStack(path: $pathStore.path) {
+            NavigationView(num: 0)
                 .navigationDestination(for: Int.self) { i in
-                    NavigationView(num: i, path: $path)
+                    NavigationView(num: i)
                 }
         }
     }

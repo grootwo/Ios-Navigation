e8bf10cacb71982ea08e82cc0742461b95244311
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-06-18 15:04:55 +0900

    Feat: set navigationbar's style

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -7,56 +7,19 @@
 
 import SwiftUI
 
-@Observable
-class PathStore {
-    var path: NavigationPath {
-        didSet {
-            save()
-        }
-    }
-
-    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
-
-    init() {
-        if let data = try? Data(contentsOf: savePath) {
-            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
-                path = NavigationPath(decoded)
-                return
-            }
-        }
-
-        // Start with an empty path.
-        path = NavigationPath()
-    }
-
-    func save() {
-        guard let representation = path.codable else { return }
-
-        do {
-            let data = try JSONEncoder().encode(representation)
-            try data.write(to: savePath)
-        } catch {
-            print("Failed to save navigation data")
-        }
-    }
-}
-
-struct NavigationView: View {
-    var num: Int
-    var body: some View {
-        NavigationLink("Going to random NavigationView", value: Int.random(in: 0...1000))
-            .navigationTitle("number \(num)")
-    }
-}
-
 struct ContentView: View {
-    @State private var pathStore = PathStore()
-    var body: some View {
-        NavigationStack(path: $pathStore.path) {
-            NavigationView(num: 0)
-                .navigationDestination(for: Int.self) { i in
-                    NavigationView(num: i)
+    var body: some View {  
+        NavigationStack {
+            List(1..<100) { i in
+                NavigationLink("\(i)") {
+                    Text("\(i) view")
                 }
+            }
+            .navigationTitle("Navigation Title")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbarBackground(.indigo, for: .navigationBar)
+            .toolbarColorScheme(.dark)
+            .toolbar(.hidden, for: .navigationBar)
         }
     }
 }
